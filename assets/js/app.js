bff8dbcb9a7063b1c226b0c62b6fa8cb2fac5fd5
= <=>
2021-01-21 19:52:47 +0300

    creating files and folders as suggested by the instructor

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var BankAccount = {
+    accountName = " ",
+    accountNUmber = " ",
+    balance = 0
+}
+(function (){
+    function createAccount(accountName,accountNUmber,balance){
+        BankAccount.accountName = accountName;
+        BankAccount.accountNUmber = accountNUmber;
+        B
+    }
+
+})();
