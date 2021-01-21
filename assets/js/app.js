2fff8ab107d24dae2a9a74fb600396e3a29de964
= <=>
2021-01-21 20:18:30 +0300

    finishing the required functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,26 @@ var BankAccount = {
         BankAccount.balance = balance
     }
     function deposit(balance){
-        BankAccount.
+        BankAccount.balance += balance
 
+    }
+    function withdraw(amount){
+        if(BankAccount.balance - amount > 50){
+            BankAccount.balance -= amount
+            return "withdrawal successful\nYour current balance is " + BankAccount.balance
+        }
+        else return "You havn't enough amount to withdraw considering the minimum amount to be stored in account is 50"
+    }
+    function balance(){
+        return "Your balance now: " + BankAccount.balance
+    }
+    function transfer(amount, transferedAccount){
+        if(BankAccount.balance - amount > 50){
+            BankAccount.balance -= amount
+            return " Transfer successfully complated you transferd\nyou transfered amount $" +amount + " to the account number " + accountNUmber
+
+        } 
+        else "You haven't enough balance to make transfer you inquired"
     }
 
 })();
